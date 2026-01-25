12ed799798e3f5f88c2f35870f52bfca80882f5c
Zachary Spar <zachspar@gmail.com>
2026-01-25 15:25:04 -0500

    chore: update ps1

--- hosts/maple/bash-config.nix
+++ hosts/maple/bash-config.nix
@@ -3,6 +3,9 @@
   programs.bash = {
     enable = true;
     shellAliases = import ./bash-aliases.nix;
+    initExtra = ''
+      PS1='\[\033[01;36m\][\[\033[01;35m\]\u\[\033[00m\]@\[\033[01;33m\]\h\[\033[01;31m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[01;36m\]]\[\033[00m\]\$ '
+    '';
   };
 }
 
