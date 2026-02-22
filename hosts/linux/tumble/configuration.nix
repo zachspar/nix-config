b4b7bc522d59009ba0cbc16bc13a285cad2374aa
Zachary Spar <zachspar@gmail.com>
2026-02-21 22:22:12 -0500

    chore: remove displaylink from imports

--- hosts/linux/tumble/configuration.nix
+++ hosts/linux/tumble/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../programs/displaylink/displaylink.nix
+      #../programs/displaylink/displaylink.nix
     ];
 
   # Bootloader.
