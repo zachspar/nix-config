23c433e250c7326a72a2f6ad75b2223a89ba979b
Zachary Spar <zachspar@gmail.com>
2026-03-22 19:51:38 -0400

    chore: go private deps

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -12,5 +12,12 @@
       push = { autoSetupRemote = true; };
       pull = { rebase = true; };
     };
+
+    # Go uses `git clone https://github.com/...` for modules; private repos need auth.
+    # With GOPRIVATE=github.com/builderhub/*, rewriting to SSH uses your normal GitHub SSH key
+    # (ssh-agent / 1Password / etc.) instead of ~/.netrc for HTTPS.
+    extraConfig = {
+      url."ssh://git@github.com/".insteadOf = "https://github.com/";
+    };
   };
 }
